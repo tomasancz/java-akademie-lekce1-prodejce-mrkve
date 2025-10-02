a073a823b1950c1ffb89e5445c50bdd391d6b33d
Tomáš B. <tomasan@email.cz>
2025-10-03 00:35:36 +0200

    Doplnění informací
    Doplnění všech zbylých informací o prodejcích

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,18 @@ public class Main {
 
         prodejce1.setJmeno("Ladislav Mrkvička");
         prodejce1.setDatumNarozeni(LocalDate.of(1939,2,2));
+        prodejce1.setPocetSmluv(30260);
+        prodejce1.setProdaneMnozstvi(1256891.35);
+        prodejce1.setSenior(true);
         prodejce2.setJmeno("Vladimír Kořen");
         prodejce2.setDatumNarozeni(LocalDate.of(1973,12,31));
+        prodejce2.setPocetSmluv(5852);
+        prodejce2.setProdaneMnozstvi(753210.92);
+        prodejce2.setSenior(false);
 
         System.out.println("Seznam akreditovaných prodejců biomrkve:");
-        System.out.println("1. " + prodejce1.getJmeno() + "; narozen " + prodejce1.getDatumNarozeni());
-        System.out.println("2. " + prodejce2.getJmeno() + "; narozen " + prodejce2.getDatumNarozeni());
+        System.out.println("1. " + prodejce1.getJmeno());
+        System.out.println("2. " + prodejce2.getJmeno());
 
     }
 }
